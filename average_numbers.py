809514fafd748f86b30b4e48e548699e0b22b2f8
Bjarni123 <Bjarni04@gmail.com>
2023-11-02 10:08:09 +0000

    komid i eina linu

--- average_numbers.py
+++ average_numbers.py
@@ -1,7 +1,3 @@
 numbersToAverage = [43, 68, 3452, 12, 69, 420]
 
-sum = 0
-for x in numbersToAverage:
-    sum += x
-
-print(sum / len(numbersToAverage))
+print(sum(numbersToAverage) / len(numbersToAverage))
