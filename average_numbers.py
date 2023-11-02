5ca9eeda1219c6e9df6e72dd9038df5a44096706
Accent8 <hreimur69@gmail.com>
2023-11-02 09:59:55 +0000

    sggdf

--- average_numbers.py
+++ average_numbers.py
@@ -1,2 +1,9 @@
 numbersToAverage = [43, 68, 3452, 12, 69, 420]
-for x in len(numbersToAverage)
+sum = 0
+count = 0
+average = 0
+for x in numbersToAverage:
+    count += 1
+    sum += x
+average = sum/count
+print(average)
